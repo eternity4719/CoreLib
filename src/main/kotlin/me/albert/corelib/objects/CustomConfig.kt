570fbb66f994bd6c67e5bc9f14108cf6660122fb
albert4719 <mcdoeswhat@qq.com>
2026-05-26 19:43:40 +0800

    Create CustomConfig.kt

---
+++ src/main/kotlin/me/albert/corelib/objects/CustomConfig.kt
@@ -0,0 +1,48 @@
+package me.albert.corelib.objects
+
+import com.google.common.base.Charsets
+import org.bukkit.configuration.file.FileConfiguration
+import org.bukkit.configuration.file.YamlConfiguration
+import org.bukkit.plugin.Plugin
+import java.io.File
+import java.io.InputStreamReader
+import java.util.logging.Level
+
+class CustomConfig(filename: String, private val plugin: Plugin) {
+
+    lateinit var config: FileConfiguration
+        private set
+
+    val configFile: File
+
+    init {
+        val file = File(plugin.dataFolder, filename)
+        if (!file.exists()) {
+            file.parentFile?.mkdirs()
+            if (plugin.getResource(filename) != null) {
+                plugin.saveResource(filename, false)
+            } else {
+                runCatching { file.createNewFile() }
+                    .onFailure { it.printStackTrace() }
+            }
+        }
+        this.configFile = file
+        reload()
+    }
+
+    fun save() {
+        runCatching {
+            config.save(configFile)
+        }.onFailure { ex ->
+            plugin.logger.log(Level.SEVERE, "Could not save config to $configFile", ex)
+        }
+    }
+
+    fun reload() {
+        config = YamlConfiguration.loadConfiguration(configFile)
+        val defConfigStream = plugin.getResource(configFile.name) ?: return
+
+        val reader = InputStreamReader(defConfigStream, Charsets.UTF_8)
+        config.setDefaults(YamlConfiguration.loadConfiguration(reader))
+    }
+}
